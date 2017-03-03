2afe7dd98b96e9e7a898163ee5fd04a7dac74a31
imanoom <i.manoom@hotmail.com>
2017-03-03 13:54:41 +0700

    no message

--- bot.php
+++ bot.php
@@ -29,6 +29,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			
 			$result = curl_exec($ch);
 			
@@ -37,7 +38,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $result
+				'text' => '1'.$result
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
