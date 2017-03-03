85d5c3a699fa9824a35654b7ec06cb3ac4d79202
imanoom <i.manoom@hotmail.com>
2017-03-03 11:52:24 +0700

    update

--- bot.php
+++ bot.php
@@ -30,12 +30,13 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			$result = curl_exec($ch);
+			var_dump($result);
 			curl_close($ch);
 			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $result
+				'text' => '1'.$result
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
