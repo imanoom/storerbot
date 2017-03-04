7af04ec24add1f979387e31aca8dec0e1db19a62
imanoom <i.manoom@hotmail.com>
2017-03-04 23:42:13 +0700

    update

--- serviceBot/bot.php
+++ serviceBot/bot.php
@@ -1,23 +1,21 @@
 <?php
 $access_token = '8I4k0Q1En3A2pfDvdmutUj0BhVqXGqjxi/EohdcFSDLcVgEYiYDvzOkAfcYPL2uEfqv0unrjH60Ka4WSXi7G//ciYEJ1WtLAF4xR02HziTya+cOGzvSk1qh2lFIiGk2PlZuFemi94zswT5WT0MPcWAdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
 $content = file_get_contents('php://input');
-// Parse JSON
+
 $events = json_decode($content, true);
-// Validate parsed JSON data
+
 if (!is_null($events['events'])) {
-	// Loop through each event
+
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
+
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
+	
 			$text = $event['message']['text'];
-			// Get replyToken
+		
 			$replyToken = $event['replyToken'];
 
-			// Send data to server
-			$url = 'http://www.alicornteam.com/storerbot/public/index.php';
+			$url = 'http://www.alicornteam.com/storerbot/routes.php';
 			$data = [
 				'userId' => $event['source']['userId'],
 				'message' => $text
@@ -38,18 +36,16 @@ if (!is_null($events['events'])) {
 			if($httpcode == 200){
 				var_dump($result);
 			} else {
-				$result = "พบข้อผิดพลาด ติดต่อมะหนุ่ม ด่วน !";
+				$result = "พบข้อผิดพลาด โปรดติดต่อร้านค้า ด่วน !";
 			}
 	  
 			curl_close($ch);
 			
-			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => $result
 			];
 			
-			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
@@ -73,27 +69,3 @@ if (!is_null($events['events'])) {
 }
 
 echo "OK";
-
-
-function get_web_page($url) {
-    $options = array(
-        CURLOPT_RETURNTRANSFER => true,   // return web page
-        CURLOPT_HEADER         => false,  // don't return headers
-        CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-        CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-        CURLOPT_ENCODING       => "",     // handle compressed
-        CURLOPT_USERAGENT      => "test", // name of client
-        CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-        CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-        CURLOPT_TIMEOUT        => 120,    // time-out on response
-    ); 
-
-    $ch = curl_init($url);
-    curl_setopt_array($ch, $options);
-
-    $content  = curl_exec($ch);
-
-    curl_close($ch);
-
-    return $content;
-}
